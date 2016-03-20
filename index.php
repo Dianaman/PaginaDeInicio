e54eb92e0249c6c894b7f12bb9998deb9f464599
Diana <diana.man.91@gmail.com>
2016-03-20 14:14:31 -0300

    `html head

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Homepage</title>
+        <link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/stylesheet.css">
+	</head>
+	<body>
+
+	</body>
+</html>
